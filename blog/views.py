556fe77850f396b4d2548361914b934886a04b27
Khanh Luc <khanh@classroom.tv>
2014-02-23 21:54:42 -0800

    updated blog. now in masonry

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import pytumblr
+
+api_key = "b3ceq6L4SAk28pxX0gpUsXZrcgtQlv82Uakpx6Ki2GIlSn5eNY";
 
 def index(request):
-    context = {"title": "Blog"}
+    client = pytumblr.TumblrRestClient(
+        consumer_key=api_key,
+    )
+
+    posts = client.posts("khanhluc.tumblr.com", filter='text')
+
+    context = {"title": "Blog", "posts": posts[u'posts'], "blog": True}
     return render(request, 'blog/index.html', context)
+
