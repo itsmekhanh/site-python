ad5273aaac60d94e378d50854a5c4105be5def32
Khanh Luc <khanh@classroom.tv>
2014-03-06 00:13:56 -0800

    updated photography app

--- photography/views.py
+++ photography/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import flickrapi
+import json
 
-# Create your views here.
+api_key = "63cee3b6bc72105b32cbed186ee52655"
+
+def index(request):
+    flickr = flickrapi.FlickrAPI(api_key, format='etree')
+    sets = flickr.photosets_getList(user_id='49585808@N08')
+    children = sets.find('photosets').findall('photoset')
+
+    context = {'title':'photography', 'sets': children}
+
+    return render(request, 'photography/index.html', context)
