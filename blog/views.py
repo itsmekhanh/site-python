57a0ad978337b98649536f568219104832376dc3
Khanh Luc <khanh@classroom.tv>
2014-03-07 15:49:30 -0800

    time to work out

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, render_to_response
-import pytumblr
+from django.utils.datetime_safe import strftime
+import pytumblr, time
 
 api_key = "b3ceq6L4SAk28pxX0gpUsXZrcgtQlv82Uakpx6Ki2GIlSn5eNY"
 tumblrName = "khanhluc.tumblr.com"
@@ -11,11 +12,32 @@ def index(request):
         consumer_key=api_key,
     )
     posts = client.posts(tumblrName, filter='text', limit=limit)
+    data = {"timeline":
+                {
+                    "headline": "Live, Love, Learn",
+                    "type": "default",
+                    "text": "The Life of a Photogrammer",
+                }
+            }
+
+    date = {}
+
+    for post in posts[u'posts']:
+        entry = {
+            "startDate":strftime("%Y,%m,%d", post[u'timestamp']),
+            "headline":"",
+            "text":post[u'text']
+        }
+        if post[u'type'] == u'video':
+            entry["asset"] = {
+                "media":post[u'permalink_url']
+            }
+
 
     context = {"title": "Blog", "posts": posts[u'posts'], "blog": True}
     return render(request, 'blog/index.html', context)
 
-def page(request, page_id):
+def page(request):
 
     if request.method == 'GET' and request.is_ajax():
         client = pytumblr.TumblrRestClient(consumer_secret=api_key)
