9de7f04c54ce1d989e4c99946224e5f729cac88c
Khanh Luc <khanh@classroom.tv>
2014-03-09 22:49:40 -0700

    galleries are up!

--- photography/urls.py
+++ photography/urls.py
@@ -7,10 +7,10 @@ from photography import views
 urlpatterns = patterns('',
     # ex: /polls/
     url(r'^$', views.index, name='index'),
+    url(r'^gallery/(?P<gallery_id>\d+)/.*$', views.gallery, name='gallery'),
     # ex: /polls/5/
     #url(r'^(?P<page_id>\d+)/$', views.page, name='page'),
     # ex: /polls/5/results/
-    # url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     # url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 )
--- photography/views.py
+++ photography/views.py
@@ -4,6 +4,7 @@ import json
 
 api_key = "63cee3b6bc72105b32cbed186ee52655"
 
+
 def index(request):
     flickr = flickrapi.FlickrAPI(api_key, format='etree')
     sets = flickr.photosets_getList(user_id='49585808@N08')
@@ -12,3 +13,17 @@ def index(request):
     context = {'photography':True, 'sets': children, 'title': "Photography"}
 
     return render(request, 'photography/index.html', context)
+
+
+def gallery(request, gallery_id):
+
+    flickr = flickrapi.FlickrAPI(api_key, format='etree')
+    try:
+        set = flickr.photosets_getPhotos(photoset_id=gallery_id)
+        context = {"set": set}
+        return render(request, 'photography/gallery.html', context)
+    except flickrapi.FlickrError:
+        return render(request, 'main/index.html')
+
+
+
