1a02fe6273cd9a4eb50f680403a99800c4dbaea8
Khanh Luc <khanh@classroom.tv>
2014-04-20 17:38:48 -0700

    updated main view

--- main/views.py
+++ main/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+import flickrapi, json
+
+api_key = "63cee3b6bc72105b32cbed186ee52655"
 
 def index(request):
-    context = {"title": "Home"}
-    return render(request, 'main/index.html', context)
+    flickr = flickrapi.FlickrAPI(api_key, format='etree')
+    sets = flickr.photosets_getList(user_id='49585808@N08', page=1, per_page=3)
 
-def blog(request):
-    context = {"title": "Blog"}
-    return render(request, '')
+    context = {"title": "Home", "featuredGalleries": sets._children[0]._children}
+    return render(request, 'main/index.html', context)
